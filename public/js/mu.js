07b59563f3d9158c6129a703077a52c23de09596
Markus Ullmark <ullmark@gmail.com>
2013-02-06 21:43:40 +0100

    added underscore

--- public/js/mu.js
+++ public/js/mu.js
@@ -3,6 +3,7 @@
 // =========
 //
 var Stage = function(options) {
+  _.bindAll(this);
   this.cubes = [];
   this.scene = new THREE.Scene();
   this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
